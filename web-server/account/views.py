2f713f9e20ebc8bf38b6a3e4c472c6c61884b4e7
Jisoo Youn <yjs930915@gmail.com>
2018-09-02 04:37:56 +0900

    밥주기

--- web-server/account/views.py
+++ web-server/account/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http.response import HttpResponseBase
 from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
 import json
 
 from .models import Account
 
+
 # Create your views here.
 @csrf_exempt
 def login(request):
-    data = {'message':""}
+    data = {'message': ""}
     kakaoID = request.POST.get('kakaoID')
 
     if not Account.objects.filter(kakaoID=kakaoID).exists():
@@ -21,6 +24,24 @@ def login(request):
     request.session['id'] = account.id
     request.session['kakaoID'] = account.kakaoID
 
-    data["level"] = account.level
+    data['account'] = model_to_dict(account)
+
+    return HttpResponse(json.dumps(data), "application/json")
+
+
+@csrf_exempt
+def feed_seolgi(request):
+    data = {'message': ""}
+
+    if not Account.objects.filter(id=request.session['id']).exists():
+        data['message'] = "Session out"
+        return HttpResponse(json.dump(data), status=401)
+    else:
+        account = Account.objects.get(id=request.session['id'])
+        exp = request.POST.get('exp')
+        account.feed(exp)
+        account.save()
+
+    data['account'] = model_to_dict(account)
     
     return HttpResponse(json.dumps(data), "application/json")
