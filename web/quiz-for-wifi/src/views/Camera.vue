5a47b2ed9d54303459e25ea7389bf1cf44d62a40
robbin <gooodev402@gmail.com>
2018-09-02 03:34:16 +0900

    web front capture camera

--- web/quiz-for-wifi/src/views/Camera.vue
+++ web/quiz-for-wifi/src/views/Camera.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="camera">
         <video class="video" autoplay></video>
+        <button id="capture" @click="captureImage()">Capture</button>
+        <canvas id="snapshot" width=320 height=240></canvas>
     </div>
 </template>
 
@@ -17,6 +19,16 @@
                     this.$el.querySelector('.video').srcObject = stream
                 }
             );
+        },
+        methods:{
+            captureImage(){
+                let canvas = this.$el.querySelector('#snapshot');
+                let context = canvas.getContext('2d');
+                let video = this.$el.querySelector('.video');
+                // Draw the video frame to the canvas.
+                context.drawImage(video, 0, 0, canvas.width,
+                    canvas.height);
+            },
         }
     }
 </script>
